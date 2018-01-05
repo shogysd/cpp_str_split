df4548a5a44441ffa56a2c2bf383d1d0c5bf5632
sh-o <shog.ysd@gmail.com>
2018-01-05 20:19:48 +0900

    Update str_split.cpp

--- str_split.cpp
+++ str_split.cpp
@@ -2,28 +2,25 @@
 
 using namespace std;
 
-
 // 回数制限なし
-void str_split(string str, string sub_str,  vector<string> &splited_strings){
-
+void str_split(string str, string sub_str, vector<string> &splited_strings) {
   int ss_len, ss_point;
 
   ss_len = sub_str.size();
 
   // 部分文字列がヒットし無くなるまで無限ループ
   while (true) {
-
     ss_point = str.find(sub_str);
 
     // 部分文字列が見つからなかった場合
-    if (ss_point == -1){
+    if (ss_point == -1) {
       break;
     }
 
     // 部分文字列が見つかった場合
-    else{
+    else {
       splited_strings.push_back(str.substr(0, ss_point));
-      str = str.substr(ss_point+ss_len, str.size()-ss_point-1);
+      str = str.substr(ss_point + ss_len, str.size() - ss_point - 1);
     }
   }
 
@@ -33,30 +30,28 @@ void str_split(string str, string sub_str,  vector<string> &splited_strings){
   return;
 }
 
-
 // 回数制限あり
-void str_split(string str, string sub_str, int max,  vector<string> &splited_strings){
-
-  int counter=0, ss_len, ss_point;
+void str_split(string str, string sub_str, int max,
+               vector<string> &splited_strings) {
+  int counter = 0, ss_len, ss_point;
 
   ss_len = sub_str.size();
 
   // 部分文字列がヒットし無くなるまで無限ループ
   while (counter < max) {
-
     ss_point = str.find(sub_str);
 
     // 部分文字列が見つからなかった場合
-    if (ss_point == -1){
+    if (ss_point == -1) {
       break;
     }
 
     // 部分文字列が見つかった場合
-    else{
+    else {
       splited_strings.push_back(str.substr(0, ss_point));
-      str = str.substr(ss_point+ss_len, str.size()-ss_point-1);
+      str = str.substr(ss_point + ss_len, str.size() - ss_point - 1);
     }
-    counter +=1;
+    counter += 1;
   }
 
   // 分割後の最後 or 分割文字列が見つからなかった時の処理
